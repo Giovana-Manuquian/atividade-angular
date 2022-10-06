150ffa29ab1ed957f8360172c9933e71d7ec0a97
Giovana-Manuquian <103048833+Giovana-Manuquian@users.noreply.github.com>
2022-10-06 17:43:39 -0300

    Update login.component.ts

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/models/user';
 import { LoginService } from 'src/app/services/login.service';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -10,16 +11,33 @@ import { LoginService } from 'src/app/services/login.service';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private loginService: LoginService) { }
+  constructor(private loginService: LoginService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
   userModel = new User();
 
+  mensagem = ""
+
   receberDados() {
     this.loginService.login(this.userModel).subscribe((response) => {
-      console.log(response)
+      console.log("Deu certo")
+      this.router.navigateByUrl("/")
+
+    }, (respostaErro) => {
+      // alert(erro.error)
+      // console.log(respostaErro.error)
+      if (respostaErro.error == 'Email and password are required') {
+        this.mensagem = 'Email e senha são obrigatórios'
+      } else if (respostaErro.error == 'Incorrect password') {
+        this.mensagem = 'Senha incorreta'
+      } else {
+        this.mensagem = respostaErro.error
+      }
+
+
+
     })
 
   }
